4fcd93d82d0c19203c38491d26784eb3d1e74362
Thyago Mota <thyagomota@gmail.com>
2025-08-04 14:46:14 -0600

    updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# Instructions
+
+Make the necessary corrections to the code, making sure you pass all tests before submitting this activity. 
---
+++ src/calculator.py
@@ -0,0 +1,27 @@
+'''
+CS3250 - Software Development Methods and Tools - Fall 2025
+Instructor: Thyago Mota
+Student:
+Description: Activity 01 - Calculator
+'''
+import math
+
+class Calculator: 
+
+    DELTA = 0.0000000001
+
+    @staticmethod
+    def add(a, b): 
+        return 0
+    
+    @staticmethod
+    def subtract(a, b): 
+        return 0
+
+    @staticmethod
+    def multiply(a, b): 
+        return 0
+
+    @staticmethod
+    def divide(a, b): 
+        return 0
